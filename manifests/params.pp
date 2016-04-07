cad745afe4bd2b5dc3610f6a828720ba656a0ed8
Jordi Prats <jprats@systemadmin.es>
2016-04-07 14:05:01 +0200

    minim

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,33 +1,18 @@
 class barman::params {
 
+  $postgres_port_default='5432'
+  #fer 9.2 minim
+
   case $::osfamily
   {
     'redhat':
     {
       case $::operatingsystemrelease
       {
-        /^[5-7].*$/:
+        /^6.*$/:
         {
         }
         default: { fail("Unsupported RHEL/CentOS version! - ${::operatingsystemrelease}")  }
-      }
-    }
-    'Debian':
-    {
-      case $::operatingsystem
-      {
-        'Ubuntu':
-        {
-          case $::operatingsystemrelease
-          {
-            /^14.*$/:
-            {
-            }
-            default: { fail("Unsupported Ubuntu version! - ${::operatingsystemrelease}")  }
-          }
-        }
-        'Debian': { fail('Unsupported')  }
-        default: { fail('Unsupported Debian flavour!')  }
       }
     }
     default: { fail('Unsupported OS!')  }
