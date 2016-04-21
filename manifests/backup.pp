71b7acdc8d6984a545ea241938f18230f02ef445
Jordi Prats <jprats@systemadmin.es>
2016-04-21 12:36:42 +0200

    variables notificació

--- manifests/backup.pp
+++ manifests/backup.pp
@@ -5,6 +5,13 @@ define barman::backup (
                         $retention_policy_mode='auto',
                         $recovery_window_days=30,
                         $user='postgres',
+                        $use_notificationscript=true,
+                        #notification script
+                        $logdir=undef,
+                        $mailto=undef,
+                        $retention='15',
+                        $idhost=undef,
+                        $compress_barmanlogfile=true,
                       ) {
   #
   file { "${barman::config::barmanconfigdir}/${backupname}.conf":
@@ -15,6 +22,15 @@ define barman::backup (
     notify  => Class['barman::service'],
     require => Class['barman::config'],
     content => template("${module_name}/backup.erb"),
+  }
+
+  if($use_notificationscript)
+  {
+    #TODO
+
+  }
+  else {
+    fail(' - Not implemented - ')
   }
 
 }
