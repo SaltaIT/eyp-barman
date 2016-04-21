4e5d6d1f58e3c6b9c8a2223de32698a854a3004f
Jordi Prats <jprats@systemadmin.es>
2016-04-21 13:43:21 +0200

    afegit notification script + config

--- manifests/backup.pp
+++ manifests/backup.pp
@@ -7,11 +7,13 @@ define barman::backup (
                         $user='postgres',
                         $use_notificationscript=true,
                         #notification script
+                        $notification_ensure='present',
                         $logdir=undef,
                         $mailto=undef,
                         $retention='15',
                         $idhost=undef,
                         $compress_barmanlogfile=true,
+                        $notificationscript_basedir='/usr/local/bin',
                       ) {
   #
   file { "${barman::config::barmanconfigdir}/${backupname}.conf":
@@ -26,7 +28,24 @@ define barman::backup (
 
   if($use_notificationscript)
   {
-    #TODO
+    file { "${notificationscript_basedir}/pgbarmanbackup.${backupname}.sh":
+      file    => $notification_ensure,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0750',
+      require => File["${barman::config::barmanconfigdir}/${backupname}.conf"],
+      content => template("${module_name}/backupscript/barmanbackup.erb"),
+    }
+
+    file { "${notificationscript_basedir}/pgbarmanbackup.${backupname}.config":
+      file    => $notification_ensure,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0640',
+      require => File["${notificationscript_basedir}/pgbarmanbackup.${backupname}.sh"],
+      content => template("${module_name}/backupscript/barmanbackupconfig.erb"),
+    }
+
 
   }
   else {
