db52e762b701c3ab6f8981a30767cffaeadac15a
Jordi Prats <jordi.prats@gmail.com>
2019-01-14 13:37:10 +0100

    demo

---
+++ examples/demo.pp
@@ -0,0 +1,25 @@
+include ::epel
+
+class { 'postgresql':
+  wal_level         => 'hot_standby',
+  max_wal_senders   => '3',
+  wal_keep_segments => '8',
+  version           => '9.6',
+  archive_mode      => true,
+  archive_dir       => '/var/lib/barman/gbm/incoming',
+  archive_dir_mode  => '0666',
+  archive_dir_chmod => '0666',
+}
+
+->
+
+class { 'barman':
+  require => Class['::epel'],
+}
+
+barman::backup { 'pgm':
+  description => 'postgres master',
+  host        => '192.168.56.29',
+  port        => '60901',
+  mailto      => 'backup_reports@systemadmin.es',
+}
