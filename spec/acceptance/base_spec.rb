ef5f2d297426f91db743d6fbbfc56fc8e83b70d6
Jordi Prats <jordi.prats@gmail.com>
2019-07-30 13:52:42 +0200

    lint

--- spec/acceptance/base_spec.rb
+++ spec/acceptance/base_spec.rb
@@ -28,9 +28,9 @@ describe 'barman class' do
 
       barman::backup { 'pgm':
         description => 'postgres master',
-        host => '192.168.56.29',
-        port => '60901',
-        mailto => 'backup_reports@systemadmin.es',
+        host        => '192.168.56.29',
+        port        => '60901',
+        mailto      => 'backup_reports@systemadmin.es',
       }
 
       EOF
